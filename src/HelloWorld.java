eb84760a19be0df96f617cbcc04f0ddc718eb5a7
Vivek Tiwari <vivektiwari7114@gmail.com>
2018-05-31 23:18:07 -0700

    added functionality to add number

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World!!");
+        int a = 3;
+        int b = 5;
+        System.out.println(a+b);
     }
 }

970bb3b83ee8037bb3096682bcefc1bc75c87f7e
Vivek Tiwari <vivektiwari7114@gmail.com>
2018-06-17 22:57:08 -0700

    Add multiplication program

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,11 @@ public class HelloWorld {
     public static void main(String args[]) {
         HelloWorld hw = new HelloWorld();
         System.out.println(hw.add(5,6));
+        System.out.println(hw.multiply(5,6));
     }
     
     public int add(int a, int b) {
         return a + b;
     }
+    public int multiply(int a, int b) {return a*b; }
 }
